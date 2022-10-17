8ebddd15ba89002e3e549524eed444af9faa8a14
Mohammad Nuridin <developer.mohammadnuridin@gmail.com>
2022-10-17 09:44:43 +0700

    country-guide is done

--- dist/main.js
+++ dist/main.js
@@ -62,6 +62,32 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
 
+var searchBtn = document.getElementById("search-btn");
+var countryInpu = document.getElementById("country-inp");
+var result = document.getElementById("result");
+var country = JSON.parse(localStorage.getItem("country-name"));
+searchBtn.addEventListener("click", function () {
+  var countryName = countryInpu.value;
+  if (!country) {
+    var finalURL = "https://restcountries.com/v3.1/name/".concat(countryName, "?fullText=true");
+    fetch(finalURL).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      localStorage.setItem("country-name", JSON.stringify(countryName));
+      !data[0] ? searchBtn.innerHTML = "<h4>Loading..</h4>" : result.innerHTML = "\n      <img src=\"".concat(data[0].flags.svg, "\" class=\"flag-img\"/>\n      <h2>").concat(data[0].name.common, "</h2>\n      <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Capital :</h4>\n                    <span>").concat(data[0].capital[0], "</span>\n          </div>\n      </div>\n      <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Continent :</h4>\n                    <span>").concat(data[0].continents[0], "</span>\n          </div>\n      </div>\n          <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Population :</h4>\n                    <span>").concat(Intl.NumberFormat("id-ID").format(data[0].population), " Jiwa</span>\n          </div>\n      </div>\n          <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Concurrency :</h4>\n                    <span>").concat(data[0].currencies[Object.keys(data[0].currencies)].name, " - ").concat(Object.keys(data[0].currencies)[0], "</span>\n          </div>\n      </div>\n          <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Common Language :</h4>\n                    <span>").concat(Object.values(data[0].languages).toString().split(",").join(","), "</span></span>\n          </div>\n      </div>\n          <div class=\"wrapper\">\n          <div class=\"data-wrapper\">\n                    <h4>Time Zone:</h4>\n                    <span>").concat(data[0].timezones[1], "</span></span>\n          </div>\n      </div>\n      ");
+    })["catch"](function () {
+      if (countryName.length == 0) {
+        result.innerHTML = "<h3>The Input field cannot be empty</h3>";
+      } else {
+        result.innerHTML = "<h3>Please enter a valid country name</h3>";
+      }
+    });
+  } else {
+    window.onload = function (event) {
+      console.log("page is fully loaded");
+    };
+  }
+});
 })();
 
 /******/ })()
